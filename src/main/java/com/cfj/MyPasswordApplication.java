45864038916e19c3a6c885a2155bde67b78c0856
dengxu <12345678>
2023-10-13 21:46:14 +0800

    初始化

---
+++ src/main/java/com/cfj/MyPasswordApplication.java
@@ -0,0 +1,20 @@
+package com.cfj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wyytcfj
+ * @Description
+ * @Date 2023/10/4
+ */
+@SpringBootApplication
+public class MyPasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPasswordApplication.class, args);
+    }
+}
+
+
+
